b53abb581c5168051723d74ce9012260ed796c09
Azizbekdevn1 <azizbekmurodov2003@gmail.com>
2024-06-08 12:02:03 +0500

    logo fix

--- root/settings.py
+++ root/settings.py
@@ -128,7 +128,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "Luch Stom Interprice",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "logo.png",
+    "site_logo": "apps/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,

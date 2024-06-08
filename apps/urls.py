0c06aed4102f9b79fc43c29c1601c38d5ab0f802
akbarali-hah <akbaralisalohiddinov808@gmail.com>
2024-06-08 12:00:29 +0500

    some updates

--- apps/urls.py
+++ apps/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from apps.views import DashboardView
 
-urlpatterns = (
-    path('', DashboardView.as_view(), name='base_dashboard_view')
-)
+urlpatterns = [
+    path('', DashboardView.as_view(), name='base_dashboard_view'),
+]

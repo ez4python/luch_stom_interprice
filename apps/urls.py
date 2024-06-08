0b438bc4fb38017f106f993c4235bef5830a4676
akbarali-hah <akbaralisalohiddinov808@gmail.com>
2024-06-08 18:06:03 +0500

    some updates

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.views import DashboardView
+from apps.views import DashboardView, ContactView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='base_dashboard_view'),
-    path('contacts/', DashboardView.as_view(), name='contact_view'),
+    path('contacts/', ContactView.as_view(), name='contact_view'),
 ]
